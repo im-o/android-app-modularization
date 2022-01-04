0c9aac73648a324401cf68fe3116e1aa883ae2df
rivaldy <frizt@gmail.com>
2022-01-04 14:27:22 +0800

    :hammer: migrate from groovy to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.3"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20"
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register(name = "type", type = Delete::class) {
+    delete(rootProject.buildDir)
 }
