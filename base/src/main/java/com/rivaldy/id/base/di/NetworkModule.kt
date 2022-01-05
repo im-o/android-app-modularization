7020084cf25ca175e0ee06f3626e66abb86bcbd4
rivaldy <frizt@gmail.com>
2022-01-05 09:21:18 +0800

    :label: add DI network module

---
+++ base/src/main/java/com/rivaldy/id/base/di/NetworkModule.kt
@@ -0,0 +1,76 @@
+package com.rivaldy.id.base.di
+
+import android.content.Context
+import com.google.gson.GsonBuilder
+import com.rivaldy.id.base.BuildConfig
+import com.rivaldy.id.base.data.network.NetworkConnectionInterceptor
+import com.rivaldy.id.base.data.remote.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by rivaldy on 05/01/22.
+ * Find me on my Github -> https://github.com/im-o
+ */
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    @Provides
+    fun provideBaseUrl(): String {
+        return BuildConfig.BASE_URL
+    }
+
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return if (BuildConfig.DEBUG) HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        else HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+
+    @Provides
+    fun providesNetworkConnectionInterceptor(@ApplicationContext context: Context): NetworkConnectionInterceptor {
+        return NetworkConnectionInterceptor(context)
+    }
+
+    @Provides
+    fun providesOkHttpClient(logging: HttpLoggingInterceptor, networkConnectionInterceptor: NetworkConnectionInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val url = chain
+                    .request()
+                    .url
+                    .newBuilder()
+                    .build()
+                chain.proceed(chain.request().newBuilder().url(url).build())
+            }
+            .addInterceptor(logging)
+            .addInterceptor(networkConnectionInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun providesConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create(GsonBuilder().create())
+    }
+
+    @Provides
+    fun providesRetrofit(baseUrl: String, converterFactory: GsonConverterFactory, okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(converterFactory)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    fun providesApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
