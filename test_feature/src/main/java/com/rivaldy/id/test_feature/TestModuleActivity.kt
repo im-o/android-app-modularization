a47d4cb2c96ae33ce2951ca25ae864f2a9a12126
rivaldy <frizgt@gmail.com>
2022-02-14 17:19:36 +0800

    :label: success show data on view

--- test_feature/src/main/java/com/rivaldy/id/test_feature/TestModuleActivity.kt
+++ test_feature/src/main/java/com/rivaldy/id/test_feature/TestModuleActivity.kt
@@ -2,9 +2,12 @@ package com.rivaldy.id.test_feature
 
 import androidx.lifecycle.lifecycleScope
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.exception.ApolloException
+import com.apollographql.apollo3.network.okHttpClient
 import com.rivaldy.id.base.BuildConfig
 import com.rivaldy.id.base.base.BaseActivity
 import com.rivaldy.id.base.data.network.DataResource
+import com.rivaldy.id.base.util.UtilExtensions.isAreVisible
 import com.rivaldy.id.base.util.UtilFunctions.loge
 import com.rivaldy.id.core.CharacterListQuery
 import com.rivaldy.id.test_feature.databinding.ActivityTestModuleBinding
@@ -24,6 +27,7 @@ class TestModuleActivity : BaseActivity<ActivityTestModuleBinding>() {
     }
     private val apolloClient = ApolloClient.Builder()
         .serverUrl("https://rickandmortyapi.com/graphql")
+        .okHttpClient(okHttpClient)
         .build()
 
     override fun getViewBinding() = ActivityTestModuleBinding.inflate(layoutInflater)
@@ -32,13 +36,18 @@ class TestModuleActivity : BaseActivity<ActivityTestModuleBinding>() {
     }
 
     override fun initObservers() {
+        binding.loadingPB.isAreVisible(true)
         lifecycleScope.launchWhenResumed {
-            try {
-                val response = apolloClient.query(CharacterListQuery()).execute()
-                loge("APOLLO : ${response.data?.characters?.results}")
-            } catch (e: Exception) {
-                loge("ERROR APOLLO : ${e}")
+            var strName = ""
+            val response = try {
+                apolloClient.query(CharacterListQuery()).execute()
+            } catch (e: ApolloException) {
+                loge("LaunchList Failure : $e")
+                null
             }
+            for (data in response?.data?.characters?.results ?: return@launchWhenResumed) strName += "${data?.name}\n"
+            binding.nameTV.text = strName
+            binding.loadingPB.isAreVisible(false)
         }
     }
 
